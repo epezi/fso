be6c23955d5d423f96ff69d6bac282d21c2243eb
mxeeri <eemeli.ristolainen@gmail.com>
2025-02-03 13:07:25 +0200

    PUT toteutettu backendiin

--- index.js
+++ index.js
@@ -26,21 +26,27 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, ) => {
+    app.get('/api/notes/:id', (request, response, next) => {
+        Person.findById(request.params.id)
+          .then(person => {
+            if (person) {
+              response.json(person)
+            } else {
+              response.status(404).end()
+            }
+          })
+      
+          .catch(error => next(error))
+      })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result=> {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
   })
 
 app.post('/api/persons',(request, response) => {
@@ -68,6 +74,18 @@ app.post('/api/persons',(request, response) => {
 
    
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
